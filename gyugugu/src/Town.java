459619a93a88889eaa4927b93117ae9783ccc373
fionam23 <144394184+fionam23@users.noreply.github.com>
2023-12-22 13:42:17 -0500

    Add files via upload
    Th-1
    Added booleans to the Hunter class representing Treasures.
    Added Treasure generation in the Town class. Each Town now generates a treasure to attribute to itself.

---
+++ gyugugu/src/Town.java
@@ -0,0 +1,211 @@
+/**
+ * The Town Class is where it all happens.
+ * The Town is designed to manage all of the things a Hunter can do in town.
+ */
+public class Town
+{
+    //instance variables
+    private Hunter hunter;
+    private Shop shop;
+    private Terrain terrain;
+    private String printMessage;
+    private boolean toughTown;
+    private int Treasure;
+
+    //Constructor
+    /**
+     * The Town Constructor takes in a shop and the surrounding terrain, but leaves the hunter as null until one arrives.
+     * @param s The town's shoppe.
+     * @param t The surrounding terrain.
+     */
+    public Town(Shop s, double toughness)
+    {
+        shop = s;
+        this.terrain = getNewTerrain();
+
+        // the hunter gets set using the hunterArrives method, which
+        // gets called from a client class
+        hunter = null;
+
+        printMessage = "";
+
+        // higher toughness = more likely to be a tough town
+        toughTown = (Math.random() < toughness);
+        Treasure = generateTreasure();
+    }
+
+    public String getLatestNews()
+    {
+        return printMessage;
+    }
+
+    /**
+     * Assigns an object to the Hunter in town.
+     * @param h The arriving Hunter.
+     */
+    public void hunterArrives(Hunter h)
+    {
+        hunter = h;
+        printMessage = "Welcome to town, " + hunter.getHunterName() + ".";
+
+        if (toughTown)
+        {
+            printMessage += "\nIt's pretty rough around here, so watch yourself.";
+        }
+        else
+        {
+            printMessage += "\nWe're just a sleepy little town with mild mannered folk.";
+        }
+    }
+
+    /**
+     * Handles the action of the Hunter leaving the town.
+     * @return true if the Hunter was able to leave town.
+     */
+    public boolean leaveTown()
+    {
+        boolean canLeaveTown = terrain.canCrossTerrain(hunter);
+        if (canLeaveTown)
+        {
+            String item = terrain.getNeededItem();
+            printMessage = "You used your " + item + " to cross the " + terrain.getTerrainName() + ".";
+            if (checkItemBreak())
+            {
+                hunter.removeItemFromKit(item);
+                printMessage += "\nUnfortunately, your " + item + " broke.";
+            }
+
+            return true;
+        }
+
+        printMessage = "You can't leave town, " + hunter.getHunterName() + ". You don't have a " + terrain.getNeededItem() + ".";
+        return false;
+    }
+
+    public void enterShop(String choice)
+    {
+        shop.enter(hunter, choice);
+    }
+
+    /**
+     * Gives the hunter a chance to fight for some gold.<p>
+     * The chances of finding a fight and winning the gold are based on the toughness of the town.<p>
+     * The tougher the town, the easier it is to find a fight, and the harder it is to win one.
+     */
+    public void lookForTrouble()
+    {
+        double noTroubleChance;
+        if (toughTown)
+        {
+            noTroubleChance = 0.66;
+        }
+        else
+        {
+            noTroubleChance = 0.33;
+        }
+
+        if (Math.random() > noTroubleChance)
+        {
+            printMessage = "You couldn't find any trouble";
+        }
+        else
+        {
+            printMessage = "You want trouble, stranger!  You got it!\nOof! Umph! Ow!\n";
+            int goldDiff = (int)(Math.random() * 10) + 1;
+            if (Math.random() > noTroubleChance)
+            {
+                printMessage += "Okay, stranger! You proved yer mettle. Here, take my gold.";
+                printMessage += "\nYou won the brawl and receive " +  goldDiff + " gold.";
+                hunter.changeGold(goldDiff);
+            }
+            else
+            {
+                printMessage += "That'll teach you to go lookin' fer trouble in MY town! Now pay up!";
+                printMessage += "\nYou lost the brawl and pay " +  goldDiff + " gold.";
+                hunter.changeGold(-1 * goldDiff);
+            }
+        }
+    }
+    public void huntForTreasure(Hunter plr){
+        if(Treasure == 1){
+            if(!plr.hasTreasure1()){
+                printMessage = "\nYou found the treasure of Ooo!";
+                plr.giveTreasure1();
+            } else {
+                printMessage = "\nYou found the treasure of Ooo! But you already have it. Discarded the dupe treasure.";
+            }
+        } else if (Treasure == 2) {
+            if(!plr.hasTreasure2()){
+                printMessage = "\nYou found the Sword of Unyielding!";
+                plr.giveTreasure2();
+            } else {
+                printMessage = "\nYou found the Sword of Unyielding! But you already have it. Discarded the dupe treasure.";
+            }
+
+        } else if (Treasure == 3) {
+            if(!plr.hasTreasure3()){
+                printMessage = "\nYou found the Crimson Flower of Death!";
+                plr.giveTreasure3();
+            } else {
+                printMessage = "\nYou found the Crimson Flower of Death! But you already have it. Discarded the dupe treasure.";
+            }
+        } else {
+            printMessage = "\nYou searched everywhere but there is no Treasure to be seen...";
+        }
+    }
+
+    public String toString()
+    {
+        return "This nice little town is surrounded by " + terrain.getTerrainName() + ".";
+    }
+
+    /**
+     * Determines the surrounding terrain for a town, and the item needed in order to cross that terrain.
+     *
+     * @return A Terrain object.
+     */
+    private Terrain getNewTerrain()
+    {
+        double rnd = Math.random();
+        if (rnd < .2)
+        {
+            return new Terrain("Mountains", "Rope");
+        }
+        else if (rnd < .4)
+        {
+            return new Terrain("Ocean", "Boat");
+        }
+        else if (rnd < .6)
+        {
+            return new Terrain("Plains", "Horse");
+        }
+        else if (rnd < .8)
+        {
+            return new Terrain("Desert", "Water");
+        }
+        else
+        {
+            return new Terrain("Jungle", "Machete");
+        }
+    }
+    private int generateTreasure(){
+        int random = (int) (Math.random()*6)+1;
+        if (random > 3){
+            return -1;
+        }
+        return random;
+    }
+    public int getTreasure(){
+        return Treasure;
+    }
+
+    /**
+     * Determines whether or not a used item has broken.
+     * @return true if the item broke.
+     */
+    private boolean checkItemBreak()
+    {
+        double rand = Math.random();
+        return (rand < 0.5);
+    }
+}
